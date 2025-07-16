0a1a2c6e019b31a50fe4f2bd2f760496b143b5a4
Ramtin Mehraz <ramtinmehraz@Ramtins-MacBook-Pro.local>
2025-07-16 12:51:46 +0330

    fix

--- classes/BotHandler.php
+++ classes/BotHandler.php
@@ -142,11 +142,12 @@ class BotHandler
                 'inline_keyboard' => $keyboard
             ];
 
+
             $this->sendRequest('editMessageText', [
                 'chat_id' => $this->chatId,
                 'text' => $text,
-               'message_id' => $messageId,
-               'reply_markup' => json_encode($reply_markup, JSON_UNESCAPED_UNICODE)
+                'message_id' => $messageId,
+                'reply_markup' => json_encode($reply_markup, JSON_UNESCAPED_UNICODE)
             ]);
 
         }
